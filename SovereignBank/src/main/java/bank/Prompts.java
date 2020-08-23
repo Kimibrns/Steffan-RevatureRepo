fa996754f7360b39a9c6c5e3fa7d207c954a2b55
Kimibrns <63929368+Kimibrns@users.noreply.github.com>
2020-08-23 11:46:53 -0400

    Update Prompts.java
    i hope this helps.  this was my understanding of how you do it

--- SovereignBank/src/main/java/bank/Prompts.java
+++ SovereignBank/src/main/java/bank/Prompts.java
@@ -14,7 +14,9 @@ public class Prompts {
 				+ "If logging in, press 1. If registering a new account, "
 				+ "press 2. To exit, press 3.");
 		Scanner userReader = new Scanner(System.in);
-		ArrayList<String> userLoginHolder = new ArrayList<String>();
+		ArrayList<String> initialuserLoginHolder = new ArrayList<String>(); //changed the name a bit
+		ArrayList<String> actualUserLoginHolder = new ArrayList<String>();  // the deserialization hold to inputs
+			
 		//String[] userLoginHolder = {"Username", "Password", "0"};
 		
 		int errorCounter = 0;
@@ -25,25 +27,48 @@ public class Prompts {
 			switch (numInput) {
 			case 1: // Login Option
 				System.out.println("Please input your username:");
-				userLoginHolder.add(userReader.next());
+				initialuserLoginHolder.add(userReader.next());
 				System.out.println("Please input your password:");
-				userLoginHolder.add(userReader.next());
+				initialuserLoginHolder.add(userReader.next());
 				Random rand = new Random();
 				int minimum = 100;
 				int randRange = 999 - minimum + 1;
-				userLoginHolder.add(Integer.toString(rand.nextInt(randRange) + minimum)); //Generates account number.
-				System.out.println(userLoginHolder); //Test output to console.
+				initialuserLoginHolder.add(Integer.toString(rand.nextInt(randRange) + minimum)); //Generates account number.
+				System.out.println(initialuserLoginHolder); //Test output to console.
 				
 				try {
-					FileOutputStream fileOut = new FileOutputStream("userlogin.txt");
+					FileOutputStream fileOut = new FileOutputStream("userlogin.ser");  //needs to be .ser to serialize
 					ObjectOutputStream out = new ObjectOutputStream(fileOut);
-					out.writeObject(userLoginHolder);
-					out.close();
+					out.writeObject(initialuserLoginHolder);
+					fileout.close(); //you forgot to close fileout
 				} catch (IOException ioe) {
 					ioe.printStackTrace();
 				}
 				
+					
+				try{
+					FileInputStream filein = new FileInputStream("userlogin.ser");
+					ObjectInputStream in = new ObjectInputStream(filein);
+					actualInitialUserHolder = (ArrayList<String>)in.readObject(); //read the data and storing it in the new array
+					in.close();
+					fiein.close();
+				}catch (IOException ioe){
+					ioe.printStackTrace();
+					return;
+				} catch(ClassNotFoundException o){
+					o.printStackTrace();
+					return;
+				}
+					
+				//seperate all the object in the one deserialization object
+				String newUserName = (String)actualInitialUserHolder.get(0);
+				String newPassword = (String)actualInitialUserHolder.get(1);
+				int newAccountNum = (int)actualInitialUserHolder.get(2);
 				
+				//print to console
+				System.out.println("Username: " +newUserName);
+				System.out.println("Password: " +newPassword);
+				System.out.println("Account Number: " +newAccountNum);
 				//Connect array with pre-existing account read from userlogin.txt.
 				errorCounter++;
 				break;
